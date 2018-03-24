ffa4a0253641f064d36534b60082af00ea749527
Dennis Christensen <dnrnmail@gmail.com>
2018-03-24 22:32:48 +0100

    fizes

--- holochain.d.ts
+++ holochain.d.ts
@@ -2,3 +2,5 @@ declare const commit: (entryType: string, entryData: any) => {};
 declare const get: (hash: string) => {};
 declare const App: any;
 declare const debug: (any);
+declare const query: (any);
+declare const getLinks: (any);
